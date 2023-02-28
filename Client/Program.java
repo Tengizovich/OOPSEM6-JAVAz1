1052929b2ba84fb320ea0e66e5d6733056f54799
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-02-28 21:09:49 +0300

    Create Program.java

---
+++ Client/Program.java
@@ -0,0 +1,11 @@
+import Homework.JAVA_OOP.Console.View;
+
+import Homework.JAVA_OOP.Presenter.Presenter;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+       Presenter p = new Presenter( new View());
+
+       p.buttonClickRN();
